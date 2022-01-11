c532824f06af097fd5eb9187af25304ce8fe4e44
jacobsonwil <jacobsonwil@urbandaleschools.com>
2022-01-11 09:30:49 -0600

    ch.09 commit

--- 9.3_Find_Function.py
+++ 9.3_Find_Function.py
@@ -31,3 +31,18 @@ Use a for loop with an index variable and a range.
 Inside the loop use an if statement. This function 
 can be written in about four lines of code.
 '''
+def find(list, k):
+    p=0
+    for i in list:
+        if i == k:
+            print("Found", k, "at position", p)
+        p += 1
+
+def main():
+    list = [36, 31, 79, 96, 36, 91, 77, 33, 19, 3, 34, 12, 70, 12, 54, 98, 86, 11, 17, 17]
+
+    find(list, 12)
+    find(list, 91)
+    find(list, 80)
+
+main()
--- 9.4_Fizzbuzz.py
+++ 9.4_Fizzbuzz.py
@@ -34,3 +34,19 @@ fizzbuzz
 
 The classic test is to use the numbers 1-100 so make sure you test that with your function.
 '''
+
+def fizzbuzz(num):
+    for i in range(1, num+1):
+        if (i % 3)==0 and i%5 != 0:
+            print("fizz")
+        elif (i % 5)== 0 and i%3 != 0:
+            print("buzz")
+        elif (i%5) == 0 and i%3 == 0:
+            print("fizzbuzz")
+        else:
+            print(i)
+
+def main():
+    fizzbuzz(100)
+
+main()
